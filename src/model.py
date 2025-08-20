e4384683ccaa881eab81207240618c0a6f1b7797
HoaTV <hoatv@gem-corp.tech>
2025-08-20 16:12:43 +0700

    add: migrate

---
+++ src/model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(30), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
